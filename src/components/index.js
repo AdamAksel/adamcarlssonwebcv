273c4631d4e65f4c66c52dde46f6ffef1a170d41
Adam Carlsson <akseltheaustrian@gmail.com>
2023-02-15 11:11:58 +0100

    work goddammit!

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Main } from './Main/Main'
-export { default as Nav } from './Nav/Nav'
-export { default as Hero } from './Hero/Hero'
-export { default as Links } from './Links/Links'
-export { default as CV } from './Links/CV/CV'
-export { default as Portfolio } from './Links/Portfolio/Portfolio'
